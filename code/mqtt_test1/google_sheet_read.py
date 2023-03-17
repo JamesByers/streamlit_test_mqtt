1e5e234ed3c0d5b2068a862b0b1240a50e93e8dc
Jim Byers <james.byers.business@gmail.com>
2023-03-17 15:16:52 -0700

    Update google_sheet_read.py

--- code/mqtt_test1/google_sheet_read.py
+++ code/mqtt_test1/google_sheet_read.py
@@ -26,7 +26,7 @@ df = df[~(df['Datetime (Pacific Time)'] < '2023-03-15 12:51')]
 df['Datetime (Pacific Time)'] = pd.to_datetime(df['Datetime (Pacific Time)'],format='%d/%m/%Y %H:%M')
 #df.index = pd.DatetimeIndex(df['Datetime (Pacific Time)'])
 df_date_index = df
-df_date_index.set_index(''Datetime (Pacific Time)'', inplace=True) 
+df_date_index.set_index(''Datetime (Pacific Time)', inplace=True) 
 dfdate_index = dfdate_index.sort_values(by='Datetime (Pacific Time)', ascending=False)
 
 st.title('Pi Pico W temperature')
