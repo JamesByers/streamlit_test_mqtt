4881f1c035e13b0123687b399899a283adc6d46c
Jim Byers <james.byers.business@gmail.com>
2023-03-17 13:32:42 -0700

    Update google_sheet_read.py

--- code/mqtt_test1/google_sheet_read.py
+++ code/mqtt_test1/google_sheet_read.py
@@ -22,6 +22,7 @@ df = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vScVe-xEahJ_RD
 #df['Datetime (Pacific Time)'] = pd.to_datetime(df['Datetime (Pacific Time)'],infer_datetime_format=True).dt.time
 #df['Datetime (Pacific Time)'] = pd.to_datetime(df['Datetime (Pacific Time)'],format='%d/%m/%Y %H:%M')
 df['Datetime (Pacific Time)'] = pd.to_datetime(df['Datetime (Pacific Time)'])
+df['Datetime (Pacific Time)'] = pd.to_datetime(df['Datetime (Pacific Time)'],format='%d/%m/%Y %H:%M')
 df = df.sort_values(by='Datetime (Pacific Time)', ascending=False)
 print(df.shape[0])
 print(df.columns)
