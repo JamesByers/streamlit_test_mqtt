1e12ed985df9f63e6491c1c4a46553102c368201
Jim Byers <james.byers.business@gmail.com>
2023-03-18 11:45:40 -0700

    added max and min by day

--- code/mqtt_test1/google_sheet_read.py
+++ code/mqtt_test1/google_sheet_read.py
@@ -19,9 +19,18 @@ st.title('My garage temperature (F)')
 st.write('As measured by a Pi Pico W running Micro Python')
 df2 = df[['Datetime (Pacific Time)','Temperature moving avg']]  #.copy
 st.line_chart(df2,x='Datetime (Pacific Time)')
-
+df_day_max =  df.groupby(pd.Grouper(key='Datetime (Pacific Time)', axis=0, 
+                      freq='1D', sort=True)).max().rename(columns={'Pi Pico Temperature (F)':'Max temperature'}).drop('Temperature moving avg', axis=1)
+df_day_max
+#df_day_max = df.groupby(df[['Datetime (Pacific Time)']].dt.day['temperature'].max())
+df_day_min =  df.groupby(pd.Grouper(key='Datetime (Pacific Time)', axis=0, 
+                      freq='1D', sort=True)).min().rename(columns={'Pi Pico Temperature (F)':'Min temperature'}).drop('Temperature moving avg', axis=1)
+st.write(df_day_min)
 
 df_date_index = df
 df_date_index.set_index('Datetime (Pacific Time)', inplace=True) 
 df_date_index = df_date_index.sort_values(by='Datetime (Pacific Time)', ascending=False)
+# applying groupby
+
+
 st.write(df_date_index.round(2))
