b53cce8a7ba6190d36a76587c7065bf757f66e51
Jim Byers <james.byers.business@gmail.com>
2023-04-07 21:36:03 -0700

    added Github link

--- code/mqtt_test1/google_sheet_001_read/google_sheet_001_read.py
+++ code/mqtt_test1/google_sheet_001_read/google_sheet_001_read.py
@@ -41,8 +41,6 @@ chart2 = alt.Chart(df_day_index, title= "Max/Min by day").mark_line().transform_
     ),
 )
 st.altair_chart(chart2, use_container_width=True)
-
-
 st.write(df_day.round(2))
 
 df_date_index = df
@@ -56,36 +54,11 @@ df_date_index = df_date_index.rename(columns={"Pi Pico Temperature (F)": "     T
 st.write(df_date_index.round(2))
 st.write('**Data Flow**')
 st.write('Sensor > Pi Pico MQTT publish > HiveMQ.cloud MQTT > Rasberry Pi Node Red > Google Sheets > Streamlit.io Python visualization')
-st.write("Code is in [this repo ](https://github.com/JamesByers/streamlit_test_mqtt/blob/main/code/mqtt_test1/google_sheet_001_read/google_sheet_001_read.py)")
-#st.write(df_date_index)
-#st.write(df_day_min.sort_index(ascending=False).round(2))
-#st.write(df_day_max.sort_index(ascending=False).round(2))
-#st.write(df_day_index)
-#df_day_index.dtypes
+st.write("See [this repo ](https://github.com/JamesByers/streamlit_test_mqtt/blob/main/code/mqtt_test1/google_sheet_001_read/google_sheet_001_read.py)")
+link="See [this repo ](https://github.com/JamesByers/streamlit_test_mqtt/blob/main/code/mqtt_test1/google_sheet_001_read/google_sheet_001_read.py) for code"
 
-#chart = alt.Chart(df_day_index).mark_line().encode(
-#    x='Datetime (Pacific Time)',
-#    y='Max temperature',
-#    #color='symbol:N',
-#)
-#chart = alt.Chart(df_day_index).mark_line().encode(
-#    x=alt.X(title="Date"),
-#    y=alt.Y(title="Temperature (F)"),
-#    mark = 'line',   
-#    line1 = alt.Chart(df_day_index.reset_index()).encode(x='Datetime (Pacific Time)', y="Max temperature"),
-#    )  
+st.markdown(link,unsafe_allow_html=True)
 
-    
-#st.altair_chart(chart)
-#, use_container_width=True)
-
-#chart = alt.Chart(df_day_index).transform_fold(
- #   ['Max temperature', 'Min temperature'], as_=['Temperature (F)']
-#).mark_line().encode(
-#    x='Datetime (Pacific Time):T',
-#    y='Temperature (F):Q',
- #   color='key:N'
-#)
 
 
 
