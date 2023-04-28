20acfec523b88fd1fe9c7141a87a4f75c144ac66
Jim Byers <james.byers.business@gmail.com>
2023-04-28 10:17:47 -0700

    updated rolling() syntax

--- code/mqtt_test1/google_sheet_read.py
+++ code/mqtt_test1/google_sheet_read.py
@@ -14,7 +14,7 @@ df = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vScVe-xEahJ_RD
 df['Datetime (Pacific Time)'] = pd.to_datetime(df['Datetime (Pacific Time)'])
 df = df[~(df['Datetime (Pacific Time)'] < '2023-03-16 00:00')]
 df['Datetime (Pacific Time)'] = pd.to_datetime(df['Datetime (Pacific Time)'],format='%d/%m/%Y %H:%M')
-df['Temperature moving avg'] = df.rolling(window=6).mean() 
+df['Temperature moving avg'] = df["Pi Pico Temperature (F)"].rolling(6).mean() 
 
 st.title('My garage temperature (F)')
 st.write('As measured every 30 minutes by a Pi Pico W running Micro Python')
