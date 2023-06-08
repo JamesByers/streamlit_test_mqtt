f60673efc695dcc9b26908d8c21ca782f0ceece7
Jim Byers <james.byers.business@gmail.com>
2023-06-08 16:42:20 -0700

    Includes some rough tries at combined chart for  BME read

--- code/mqtt_test1/google_sheet_read_bme/google_sheet_read_bme.py
+++ code/mqtt_test1/google_sheet_read_bme/google_sheet_read_bme.py
@@ -12,6 +12,8 @@ st.write('Measured by a Pi Pico W, a BME280 sensor, and MicroPython')
 st.write('Updated every 30 min')
 st.write('')
 
+import streamlit as st
+
 # Read in and prep raw data
 df = pd.read_csv("https://docs.google.com/spreadsheets/d/1iSZacyhgC-hp35HY-fwOgzEgPoj22BhM49HSh325-8o/gviz/tq?tqx=out:csv&sheet=raw_data")
 df['Datetime PT'] = pd.to_datetime(df['Datetime (Pacific Time)'])
@@ -23,16 +25,65 @@ df['Moving avg (3)'] = df["BME Temp (F)"].rolling(3).mean()
 # Publish chart of temperature over time
 df2 = df[['Datetime PT','BME Temp (F)']]
 df2['Datetime PT'] = df2['Datetime PT'] + pd.DateOffset(hours=7)  
-chart1 = alt.Chart(df2, title= "Back Porch Temperature").mark_line().encode(
+temperature_chart = alt.Chart(df2, title= "Temperature").transform_calculate(
+    hot = 'datum["BME Temp (F)"] >=75.0'
+).mark_line().encode(
+
+#temperature_chart = alt.Chart(df2, title= "Temperature").mark_line().encode(
     x=alt.X('Datetime PT:T', axis=alt.Axis(format="%-m/%-d/%y", tickCount="day", title=None)),
-    y=alt.Y('BME Temp (F):Q', title= "Degrees F"),
+    y=alt.Y('BME Temp (F):Q', title= "Degrees F", impute={'value': 75.0}),
+    color = ('hot:N'), #, alt.Legend=None},
+    #legend=None,
     tooltip=[
        alt.Tooltip('Datetime PT', format="%-m/%-d/%-y %-I:%-M %p", title="Time PT"),
-       #alt.Tooltip('Datetime PT', format="%-H:%-M %p", title="Time"),
        alt.Tooltip('BME Temp (F)', format=".1f", title="Temp (F)"),
-    ]
+    ],
+#    color=alt.Color('BME Temp (F):Q',
+#            scale=alt.Scale(
+ #              domain=[0,75], 
+ #               range=['blue','red'], 
+ #               interpolate=None
+#                ),
+
+#    color = alt.condition('datum["BME Temp (F)"] >70', 
+ #                          alt.value('red'), 
+ #                          alt.value('green')
+ #   ),
+ ).configure_range(
+        category={'scheme': 'category10'}
 )
-st.altair_chart(chart1, use_container_width=True)
+st.altair_chart(temperature_chart, use_container_width=True)
+
+#alt.Chart(df).transform_calculate(
+#    negative='datum.y < 0'
+#).mark_area().encode(
+#    x='x',
+#    y=alt.Y('y', impute={'value': 0}),
+#    color='negative:N'
+#)
+'''
+# Chart temp and humidity together
+df_temp = df[['Datetime PT','BME Temp (F)', 'Humidity']]
+base = alt.Chart(df_temp).encode(
+    alt.X('Datetime PT:T')#.axis(title=None) #'Temperature and Humidity'))
+)
+temperature_values = base.mark_line(stroke='#5276A7', interpolate='monotone').encode(
+    alt.Y('BME Temp (F):Q')#.title('Temperature (F)', titleColor='#57A44C'),
+)
+
+humidity_values = base.mark_line(stroke='#5276A7', interpolate='monotone').encode(
+    alt.Y2('Humidity:Q'), titleColor='#5276A7',)
+
+alt.layer(temperature_values, humidity_values).resolve_scale(
+     y='independent'
+)
+#st.altair_chart(chart3) #.resolve_scale(y='independent'))temperature_values + humidity_values
+
+#st.altair_chart(chart3, use_container_width=True)
+
+'''
+
+#st.altair_chart(temperature_chart,humidity_chart.resolve_scale(y='independent')) #temperature_values + humidity_values
 
 # Create Max/Min by day dataframe
 df3 = df
@@ -44,12 +95,10 @@ df_day = pd.concat([df_day_min, df_day_max], axis=1).sort_index(ascending=False)
 df_day.index.rename('Date', inplace= True)
 df_day.sort_index()
 
-
-
 # Publish Max/Min by day chart
 df_day_index = df_day.reset_index()
 df_day_index['Date'] = df_day_index['Date'] + pd.DateOffset(days=1) # This correction was needed for correct tooltip date but not clear why.
-chart2 = alt.Chart(df_day_index, title= "Max/Min by day").mark_line().transform_fold(
+chart2 = alt.Chart(df_day_index, title= "Temperature Max/Min by day").mark_line().transform_fold(
     fold=['Max temp','Min temp'], 
     as_=['variable', 'value']
 ).encode(
@@ -64,6 +113,20 @@ chart2 = alt.Chart(df_day_index, title= "Max/Min by day").mark_line().transform_
     ]
 )
 st.altair_chart(chart2, use_container_width=True)
+
+#Chart of humidity over time
+df_temp= df[['Datetime PT','Humidity']]
+df_temp['Datetime PT'] = df_temp['Datetime PT'] + pd.DateOffset(hours=7)  
+humidity_chart = alt.Chart(df_temp, title= "Humidity").mark_line().encode(
+    x=alt.X('Datetime PT:T', axis=alt.Axis(format="%-m/%-d/%y", tickCount="day", title=None)),
+    y=alt.Y('Humidity:Q', title= "Humidity"),
+    color = alt.value('green'),
+    tooltip=[
+       alt.Tooltip('Datetime PT', format="%-m/%-d/%-y %-I:%-M %p", title="Time PT"),
+       alt.Tooltip('Humidity', format=".1f", title="Humidity"),
+    ]
+)
+st.altair_chart(humidity_chart, use_container_width=True)
 #
 # Write table of daily Max/Min values
 df4 = df_day
