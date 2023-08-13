6bf6de3a4f03569a9ce64a796627cccec0dd8e0b
Jim Byers <james.byers.business@gmail.com>
2023-08-12 20:49:58 -0700

    Corrected humidity at top so properly rounded

--- code/mqtt_test1/google_sheet_read_bme/google_sheet_read_bme.py
+++ code/mqtt_test1/google_sheet_read_bme/google_sheet_read_bme.py
@@ -44,7 +44,7 @@ else :
     
 st.markdown(f"""
   ### Temperature: <span style="color:#1f77b4">{int(round(current_temperature, 0))} &deg;F</span>
-  ### Humidity:    <span style="color:#1f77b4">{round(current_humidity, 0)}%</span> 
+  ### Humidity:    <span style="color:#1f77b4">{int(round(current_humidity, 0))}%</span> 
   ### Barametric Pressure: <span style="color:#1f77b4">{round(current_pressure,1)} </span><span style="color:{pressure_color}">{pressure_trend} </span>
   #### 
 """, unsafe_allow_html=True
